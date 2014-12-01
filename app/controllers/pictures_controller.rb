cf408b0cf140cd83094fbf3de45af89aa46ee8c1
hellosamkim <sam.mt.kim@gmail.com>
2014-12-01 18:37:39 -0500

    Each picture has its own view

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -12,4 +12,18 @@ class PicturesController < ApplicationController
       }
     ]
   end
+  def show
+    @pictures = [
+      {
+        :url    => "http://cdn.picography.co/kings-row.jpg"
+      },
+      {
+        :url    => "http://cdn.picography.co/coola-boola.jpg"
+      },
+      {
+        :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
+      }
+    ]
+    @picture = @pictures[params[:id].to_i]
+  end
 end
