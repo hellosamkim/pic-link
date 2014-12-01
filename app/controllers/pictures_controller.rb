c8d8f5b3780a791b61b0a99915ad6f59a5a9061e
hellosamkim <sam.mt.kim@gmail.com>
2014-12-01 18:25:15 -0500

    preloaded a few pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,2 +1,15 @@
 class PicturesController < ApplicationController
+  def index
+    @pictures = [
+      {
+        :url    => "http://cdn.picography.co/kings-row.jpg"
+      },
+      {
+        :url    => "http://cdn.picography.co/coola-boola.jpg"
+      },
+      {
+        :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
+      }
+    ]
+  end
 end
