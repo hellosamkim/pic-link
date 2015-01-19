2830cc8ab70f9d924cadfe982a6f67d91f67825b
hellosamkim <sam.mt.kim@gmail.com>
2015-01-19 11:36:59 -0500

    Picture links are working again

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,51 +5,50 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
 Picture.create!(
-  :url => "http://cdn.picography.co/kings-row.jpg"
+  :url => "https://unsplash.imgix.net/photo-1419332563740-42322047ff09?q=75&fm=jpg&s=f1e7d176e154bfe4a511b9ecd95a0a84"
 )
 
 Picture.create!(
-  :url => "http://cdn.picography.co/coola-boola.jpg"
+  :url => "https://unsplash.imgix.net/reserve/OlxPGKgRUaX0E1hg3b3X_Dumbo.JPG?q=75&fm=jpg&s=07d78270aff780117b8a02cee4252ea2"
 )
 
 Picture.create!(
-  :url => "http://cdn.picography.co/trevi-fountain.jpg"
+  :url => "https://unsplash.imgix.net/photo-1415226581130-91cb7f52f078?q=75&fm=jpg&s=859a0a6f863f8eb93a489c98402e687b"
 )
 
 Picture.create!(
-  :url => "http://cdn.picography.co/the-look.jpg"
+  :url => "https://ununsplash.imgix.net/uploads/1411589111646628d45fa/12f0a18c?q=75&fm=jpg&s=254613954431e56bf4d917ad54a56b03"
 )
 
 Picture.create!(
-  :url => "http://cdn.picography.co/boats-of-colour.jpg"
+  :url => "https://unsplash.imgix.net/reserve/PPE2xapKRNyy2DlTt89F_Gutman_island.jpg?q=75&fm=jpg&s=d65d0e7f397d2a59c93dbd0aa4b29603"
 )
 
 Picture.create!(
-  :url => "http://cdn.picography.co/dalkey-island.jpg"
+  :url => "https://s3.amazonaws.com/ooomf-com-files/nQZcA7PRTyuduZPSZQ88_wanderlust.jpg"
 )
 
 Picture.create!(
-  :url => "http://cdn.picography.co/life-line.jpg"
+  :url => "https://s3.amazonaws.com/ooomf-com-files/Vo7YbYQQ8iyOo4J9bOoj_ggb24.jpg"
 )
 
 Picture.create!(
-  :url => "http://cdn.picography.co/break-dancer.jpg"
+  :url => "https://s3.amazonaws.com/ooomf-com-files/E5KMvPp9SsCnqmEGUwAS_2014-08-10%2012.56.40%201.jpg"
 )
 
 Picture.create!(
-  :url => "http://cdn.picography.co/seahorse.jpg"
+  :url => "https://s3.amazonaws.com/ooomf-com-files/U7Fc1sy5SCUDIu4tlJY3_NY_by_PhilippHenzler_philmotion.de.jpg"
 )
 
 Picture.create!(
-  :url => "http://cdn.picography.co/he-is-watching.jpg"
+  :url => "https://s3.amazonaws.com/ooomf-com-files/KgqDYXRLSeyeSbhxYTjK_lookingAtTheArch.jpg"
 )
 
 Picture.create!(
-  :url => "http://cdn.picography.co/chrome-mercedes.jpg"
+  :url => "https://s3.amazonaws.com/ooomf-com-files/pHyYeNZMRFOIRpYeW7X3_manacloseup%20copy.jpg"
 )
 
 Picture.create!(
-  :url => "http://cdn.picography.co/street-artists.jpg"
+  :url => "https://s3.amazonaws.com/ooomf-com-files/483LKAktTE2ZB1UIkNYY_3.jpg"
 )
